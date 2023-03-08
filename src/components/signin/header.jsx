b697a924a1343b0eb96ba2bd6443f0cd6aeb03d9
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-03-08 22:11:08 +0100

    working on signing In

---
+++ src/components/signin/header.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Header() {
+    return (
+        <header className="flex justify-between content-baseline py-6 sm:px-8 px-5 mb-[70px]">
+            <Link href='/' className="2xl:w-[167px] xl:w-[134px] lg:w-[108px] w-[30%] 2xl:h-[49px] xl:h-[36px] lg:h-[32px] h-[24px] flex justify-start mt-[10px] relative">
+                <Image className="object-scale-down absolute top-0 bottom-0 w-full h-full"
+                    src='/assets/Logo.png'
+                    alt="netflix logo"
+                    fill
+                />
+            </Link>
+        </header>
+    )
+}
--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,6 +1,21 @@
+import Head from "next/head";
+import Header from "@/components/signin/header";
+
 
 export default function signIn() {
-  return (
-    <div>signIn</div>
-  )
+    return (
+        <>
+            <Head>
+                <title>Netflix clone signIn</title>
+                <meta name="description" content="Netflix clone app" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/4375011_logo_netflix_icon.png" />
+            </Head>
+            <main className="absolute top-0 bottom-0 w-full herosection">
+                <div className="absolute h-full w-full bg-black/60">
+                    <Header />
+                </div>
+            </main>
+        </>
+    )
 }
