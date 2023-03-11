1c6f70073f60c41363d0c83fa6596424089810b8
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-03-11 20:25:51 +0100

    added line heights to value props sections

--- src/components/landing-page/valueProps2.jsx
+++ src/components/landing-page/valueProps2.jsx
@@ -5,7 +5,7 @@ export default function ValueProp2() {
         <section className="bg-black py-[30px] text-white">
             <div className="2xl:w-3/4 xl:w-4/5 w-11/12 mx-auto flex lg:flex-row-reverse flex-col">
                 <div className="lg:pt-[112px] lg:pb-[112px] pt-[45px] pb-0 lg:pr-16 lg:w-7/12 w-full">
-                    <h3 className="lg:text-[50px] sm:text-[2.5rem] leading-[60px] text-2xl font-black lg:mb-8 mb-6 lg:text-start text-center">Download your shows to watch offline.</h3>
+                    <h3 className="lg:text-[50px] sm:text-[2.5rem] sm:leading-[60px] text-2xl font-black lg:mb-8 mb-6 lg:text-start text-center">Download your shows to watch offline.</h3>
                     <p className="lg:text-[26px] sm:text-xl text-lg lg:text-start text-center">Save your favorites easily and always have something to watch.</p>
                 </div>
                 <div className="relative lg:w-[530px] sm:w-2/3 w-full lg:mx-0 mx-auto lg:h-[398px] h-[400px] sm:mt-0">
